40515d8dd274f77ab37e7afd7de9e5b1ba891c16
Bogdan Polak <bogdanpol@gmail.com>
2020-05-09 20:22:01 +0100

    first commit

---
+++ src/Code.DelphiChallenge01.pas
@@ -0,0 +1,31 @@
+﻿unit Code.DelphiChallenge01;
+
+interface
+
+{
+@subject: Delphi Challenge #01
+@author: Jacke Laskiwski
+@date: 2020-05-09 21:00
+
+Funkcja przyjmująca na wejściu dwa parametry:
+  - łańcuch tekstowy oraz
+  - znak (char),
+celem funkcji jest usunięcie z zadanego łańcucha wszystkich powtórzonych
+znaków zgodnych z podanym charem i pozostawienie go tylko pojedynczo.
+
+Przykładowo przekazuję do funkcji łańcuch:
+  "Wlazł koooootek na płoooooootek i mruga",
+  oraz jako char literę "o",
+  a funkcja zwraca: "Wlazł kotek na płotek i mruga".
+}
+
+function Challenge01(const aText:string; const aChar:char): string;
+
+implementation
+
+function Challenge01(const aText:string; const aChar:char): string;
+begin
+  Result := '';
+end;
+
+end.
---
+++ src/Test.DelphiChallenge01.pas
@@ -0,0 +1,28 @@
+﻿unit Test.DelphiChallenge01;
+
+interface
+uses
+  DUnitX.TestFramework;
+
+type
+
+  [TestFixture]
+  TDelphiChallenge01 = class(TObject) 
+  published
+    procedure Test1;
+  end;
+
+implementation
+
+uses Code.DelphiChallenge01;
+
+procedure TDelphiChallenge01.Test1;
+begin
+  Assert.AreEqual (
+    'Wlazł kotek na płotek i mruga',
+    Challenge01('Wlazł koooootek na płoooooootek i mruga', 'o'));
+end;
+
+initialization
+  TDUnitX.RegisterTestFixture(TDelphiChallenge01);
+end.
