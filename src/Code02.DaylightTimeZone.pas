808b29272f6ed7fce3cef669d436ea919e44c481
Jacek Laskowski <j.laskowski@sokaris.pl>
2020-05-17 12:03:18 +0200

    in progress

--- src/Code02.DaylightTimeZone.pas
+++ src/Code02.DaylightTimeZone.pas
@@ -2,6 +2,8 @@
 
 interface
 
+uses
+  System.Generics.Collections;
 {
 @theme: Delphi Challenge
 @subject: #02 Daylight Time Zone
@@ -39,29 +41,156 @@ interface
     aHtmlPageContent := TMyHttpGet.GetWebsiteContent(‘http://delphi.pl/’);
 }
 
-function IsDaylightSaving(const area: string; year: word): boolean;
-function GetDaylightStart(const area: string; year: word): TDateTime;
-function GetDaylightEnd(const area: string; year: word): TDateTime;
+
+type
+  TTimeAreaPoint = type string;
+
+  TTimeAreaPointHelper = record helper for TTimeAreaPoint
+  protected
+    function GetYear(): Word;
+    procedure SetYear(const aValue: Word);
+    function GetArea(): string;
+    procedure SetArea(const aValue: string);
+  public
+    property Area: string read GetArea write SetArea;
+    property Year: Word read GetYear write SetYear;
+  end;
+
+  TDayligtSavingPeriod = record
+    StartPeriod : TDateTime;
+    EndPeriod : TDateTime;
+  end;
+
+  TDaylightSavingMatrix = record
+  private
+    const cReg = 'mgt0.*?reach<br>(.*?)[^0-9]*<strong>(.*?)</strong>';
+    const cNotDST  = 'Daylight Saving Time (DST) Not Observed in Year ';
+    const cURL = 'https://www.timeanddate.com/time/change/%s?year=%d';
+    class var cfDLSMatrix : TDictionary<TTimeAreaPoint, TDayligtSavingPeriod>;
+    class function ImportPeriod(const aArea: string; aYear: Word): TDayligtSavingPeriod; static;
+  public
+    class function GetPeriod(const aArea: string; aYear: Word): TDayligtSavingPeriod; static;
+    class constructor Create();
+    class destructor Destroy();
+  end;
+
+function IsDaylightSaving(const area: string; year: Word): boolean;
+function GetDaylightStart(const area: string; year: Word): TDateTime;
+function GetDaylightEnd(const area: string; year: Word): TDateTime;
 
 implementation
 
 uses
-  System.SysUtils,
-  Code02.HttpGet;
+  Code02.HttpGet,
 
-function IsDaylightSaving(const area: string; year: word): boolean;
+  System.RegularExpressions,
+  System.SysUtils;
+
+function IsDaylightSaving(const area: string; year: Word): boolean;
+var
+  lPeriod: TDayligtSavingPeriod;
 begin
-  Result:= False;
+  lPeriod := TDaylightSavingMatrix.GetPeriod(area, year);
+  Result := (lPeriod.StartPeriod > 0) and (lPeriod.EndPeriod > 0);
 end;
 
-function GetDaylightStart(const area: string; year: word): TDateTime;
+function GetDaylightStart(const area: string; year: Word): TDateTime;
 begin
   Result := 0;
 end;
 
-function GetDaylightEnd(const area: string; year: word): TDateTime;
+function GetDaylightEnd(const area: string; year: Word): TDateTime;
 begin
   Result := 0;
+end;
+
+function TTimeAreaPointHelper.GetArea(): string;
+begin
+  if Length(Self) < 1 then
+  begin
+    Result := '';
+  end else
+  begin
+    Result := Copy(Self, 2);
+  end;
+end;
+
+function TTimeAreaPointHelper.GetYear(): Word;
+begin
+  if Length(self) = 0 then
+  begin
+    Result := 0;
+  end else
+  begin
+    Result := Word(Self[1]);
+  end;
+end;
+
+procedure TTimeAreaPointHelper.SetArea(const aValue: string);
+begin
+  if Length(Self) = 0 then
+  begin
+    Self := #0 + aValue;
+  end else
+  begin
+    Self := Self[1] + aValue;
+  end;
+end;
+
+procedure TTimeAreaPointHelper.SetYear(const aValue: Word);
+begin
+  if Length(Self) = 0 then
+  begin
+    SetLength(Self, 1);
+  end;
+  Self[1] := Char(aValue);
+end;
+
+class constructor TDaylightSavingMatrix.Create();
+begin
+  cfDLSMatrix := TDictionary<TTimeAreaPoint, TDayligtSavingPeriod>.Create();
+end;
+
+class destructor TDaylightSavingMatrix.Destroy();
+begin
+  cfDLSMatrix.Free;
+end;
+
+class function TDaylightSavingMatrix.GetPeriod(const aArea: string; aYear: Word): TDayligtSavingPeriod;
+var
+  lPoint : TTimeAreaPoint;
+begin
+  lPoint.Area := aArea;
+  lPoint.Year := aYear;
+  if cfDLSMatrix.TryGetValue(lPoint, Result) then
+  begin
+    Exit;
+  end;
+  Result := ImportPeriod(aArea, aYear);
+  cfDLSMatrix.Add(lPoint, Result);
+end;
+
+class function TDaylightSavingMatrix.ImportPeriod(const aArea: string; aYear: Word): TDayligtSavingPeriod;
+var
+  lArea: string;
+  lBody: string;
+  lMatches: TMatchCollection;
+  lRegex: TRegEx;
+  lURL : string;
+begin
+  lArea := aArea.ToLower;
+  lURL := Format(cURL, [lArea, aYear]);
+  lBody := TMyHttpGet.GetWebsiteContent(lURL);
+  if Pos(cNotDST, lBody) > 0 then
+  begin
+    Result.StartPeriod := 0;
+    Result.EndPeriod := 0;
+  end else
+  begin
+    lMatches := TRegEx.Matches(lBody, cReg, [roIgnoreCase, roMultiLine]);
+    Result.StartPeriod := 0;
+    Result.EndPeriod := 0;
+  end;
 end;
 
 end.
